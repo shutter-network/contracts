cd2c59e68d63c03976d851c93b83c8697bb042bb
Konrad Feldmeier <konrad@brainbot.com>
2024-08-19 14:56:55 +0200

    Initial shop bindings

---
+++ gen_shop_bindings.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+CONTRACTS=(
+    "Inbox"
+    "ValidatorRegistry"
+    "KeyperSetManager"
+    "KeyperSet"
+    "EonKeyPublish"
+    "KeyBroadcastContract"
+)
+OUTPUT_DIR="shopcontracts"
+PACKAGE_NAME="shopcontracts"
+
+mkdir -p "$OUTPUT_DIR"
+
+forge build
+
+for contract in "${CONTRACTS[@]}"; do
+    pkg=$(echo "$contract" | tr '[:upper:]' '[:lower:]')
+    d="${OUTPUT_DIR}/${pkg}"
+    mkdir -p "${d}"
+    abigen --abi <(jq '.["abi"]' "out/${contract}.sol/${contract}.json") --pkg "${pkg}" --out "${d}/${pkg}.go"
+done
